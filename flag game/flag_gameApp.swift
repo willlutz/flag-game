6f46cfc439772e822db861f237ecaa3b1d48ec4f
will lutz <wlutz@bsd220.org>
2024-01-09 09:34:52 -0600

    Initial Commit

---
+++ flag game/flag_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  flag_gameApp.swift
+//  flag game
+//
+//  Created by will lutz on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct flag_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
