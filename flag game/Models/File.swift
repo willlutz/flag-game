aba2ef40c5c321d81e11c26ffd069a52aae90249
will lutz <wlutz@bsd220.org>
2024-01-17 10:03:03 -0600

    game manager

--- flag game/Models/File.swift
+++ flag game/Models/File.swift
@@ -7,29 +7,26 @@
 
 import Foundation
 struct Data {
-    let questions = [
-        Question(correctAnswer:
-                Answer(text: "England", isCorrect: true),
-              incorrectAnswers: [
-                Answer(text: "Ireland", isCorrect: false),
-                Answer(text: "Iceland", isCorrect: false),
-                Answer(text: "South Africa", isCorrect: false)
-              ])
-        ,
-        Question(correctAnswer:
-                Answer(text: "France", isCorrect: true),
-           incorrectAnswers: [
-                Answer(text: "Spain", isCorrect: false),
-                Answer(text: "Italy", isCorrect: false),
-                Answer(text: "Germany", isCorrect: false)
-           ])
-        ,
-        Question(correctAnswer:
-                Answer(text: "Germany", isCorrect: true),
-           incorrectAnswers: [
-                Answer(text: "Austria", isCorrect: true),
-                Answer(text: "Italy", isCorrect: false),
-                Answer(text: "Czech Republic", isCorrect: false)
-           ])
-    ]
+    let countries = [
+           "Angola",
+           "Austria",
+           "China",
+           "England",
+           "Estonia",
+           "France",
+           "Germany",
+           "Greece",
+           "India",
+           "Israel",
+           "Italy",
+           "Norway",
+           "Pakistan",
+           "Peru",
+           "Saudi Arabia",
+           "South Africa",
+           "Spain",
+           "Sweden",
+           "Ukraine",
+           "United States"
+       ]
 }
